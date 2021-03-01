b4affcfc17bc230591f315a6a2b76d3e203e5be0
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-MacBook-Pro.local>
2021-02-28 22:22:35 -0500

    Minimal package TS code

---
+++ ticketing/auth/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+import { json } from "body-parser";
+
+const PORT = 3000;
+
+const app = express();
+app.use(json());
+
+app.listen(PORT, () => {
+  console.log(`Action on PORT: ${PORT}`);
+});
