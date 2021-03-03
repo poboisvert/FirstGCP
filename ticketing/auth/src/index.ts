042d8f1f229347ec2dc5dcfd98c0739bb12a28e4
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-MacBook-Pro.local>
2021-03-02 20:25:41 -0500

    express-async-errors & errorHandler

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,5 +1,6 @@
 // Dependencies
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 // npmjs library module
 
@@ -8,6 +9,7 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middleware/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const PORT = 3000;
 const app = express();
@@ -19,14 +21,18 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+// Async will return a promise without it return values
+// Await, Async with package express-async-errors un /auth
+app.all("*", async (req, res, next) => {
+  throw new NotFoundError();
+});
+
 // Middleware to return error messages - uniform
 app.use(errorHandler);
 
 // From BROWSER to API
 /* app.get("/api/users/currentuser", (req, res) => {
-  res.send("This is a working test - PAL");
-
-  
+  res.send("This is a working test - PAL");  
 }); */
 
 app.listen(PORT, () => {
