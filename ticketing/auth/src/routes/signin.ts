3495cd189b199da9f26e24e9e2e1b45ee1877de5
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-MacBook-Pro.local>
2021-03-08 17:55:42 -0500

    curr user & Postman https

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,30 +1,22 @@
-// Import Request, Response for Typescript
 import express, { Request, Response } from "express";
-// body is used as a middleware and validate the information
-import { body, validationResult } from "express-validator";
-// Middleware error handler
-import { validateRequest } from "../middlewares/validate-request";
-// ERRORS template
-import { BadRequestError } from "../errors/bad-request-error";
-// Import user model
-import { User } from "../models/user";
-import { Password } from "../services/password";
-// JWT session
+import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 
-// Setup router
+import { Password } from "../services/password";
+import { User } from "../models/user";
+import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
+
 const router = express.Router();
 
-// Action from router
 router.post(
   "/api/users/signin",
   [
-    // Middleware validation fields
     body("email").isEmail().withMessage("Email must be valid"),
     body("password")
       .trim()
       .notEmpty()
-      .withMessage("Password must be between 4 and 20 characters"),
+      .withMessage("You must supply a password"),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
@@ -40,13 +32,9 @@ router.post(
       password
     );
 
-    if (!passwordsMatch) {
-      throw new BadRequestError("Invalid Credentials");
-    }
-
     // Compare the user from the req.password and the MongoDB
     if (!passwordsMatch) {
-      throw new BadRequestError("Invalid credentials");
+      throw new BadRequestError("Invalid Credentials");
     }
 
     //
@@ -65,10 +53,8 @@ router.post(
       jwt: userJwt,
     };
 
-    // Mongo DB return _id instead of id
     res.status(200).send(existingUser);
   }
 );
 
-// Export the route and rename. Can't use router for all
 export { router as signinRouter };
