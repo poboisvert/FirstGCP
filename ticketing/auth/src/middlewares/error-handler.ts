bcde2ec78ed539b33174aef8061d154f77da2bee
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-23 21:35:14 -0400

    Test invalid email

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -12,6 +12,7 @@ export const errorHandler = (
   // https://en.wikipedia.org/wiki/List_of_HTTP_status_codes - 500 internal error or 503
   if (err instanceof CustomError) {
     // 400 user sent bad data
+    // console.log(err)
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
--- ticketing/auth/src/routes/__test__/signup.test.ts
+++ ticketing/auth/src/routes/__test__/signup.test.ts
@@ -4,6 +4,13 @@ import { app } from "../../app";
 it("Returns a 201 on successful signup", async () => {
   return request(app)
     .post("/api/users/signup")
-    .send({ email: "test@test.com", password: "pasword" })
+    .send({ email: "test@test.com", password: "password" })
     .expect(201);
+});
+
+it("Returns a 400 on invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({ email: "test.com", password: "password" })
+    .expect(400);
 });
