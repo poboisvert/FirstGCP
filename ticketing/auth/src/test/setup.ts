06c34f122fad906c4b84abd1d6b9fffc30249a32
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-23 23:18:15 -0400

    Cookie Global

--- ticketing/auth/src/test/setup.ts
+++ ticketing/auth/src/test/setup.ts
@@ -1,6 +1,15 @@
+import request from "supertest";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { app } from "../app";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 // Run before all test
 let mongo: any;
@@ -29,3 +38,17 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+// Test global signin
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const res = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = res.get("Set-Cookie");
+  return cookie;
+};
