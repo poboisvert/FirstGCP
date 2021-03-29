4561101f9493ac0fb81fc897ebfc83fed85e11ab
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-03-28 22:53:20 -0400

    Cookie helper API

--- ticketing/client/pages/index.js
+++ ticketing/client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   // console.log(currentUser);
@@ -8,29 +8,17 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  console.log(req.headers); // cookie for the session will be displayed
+LandingPage.getInitialProps = async (context) => {
+  //console.log(req.headers); // cookie for the session will be displayed
   // const response = await axios.get('/api/users/currentuser');
   // k get services -n ingress-nginx
   // https://nextjs.org/docs/api-reference/data-fetching/getInitialProps
   // return response.data;
   console.log("Running Index");
 
-  if (typeof windows === "undefined") {
-    // Server Side - Request to forward to Ingress NGINX
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: req.headers,
-      }
-    );
-    return data;
-  } else {
-    // Browser side
-    const { data } = await axios.get("/api/users/currentuser");
-    return data;
-  }
-  return {};
+  const { data } = await buildClient(context).get("/api/users/currentuser");
+
+  return data;
 };
 
 export default LandingPage;
