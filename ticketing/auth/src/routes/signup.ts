47e7c17ed39289fd4e28c85448ff0e3ac9d47124
Pierre-Olivier Boisvert <pob944@hotmail.com>
2021-04-04 15:43:52 -0400

    Shared NPM publish

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -2,9 +2,7 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 import { validateRequest, BadRequestError } from "@bonnethood/common";
-
 import { User } from "../models/user";
-
 const router = express.Router();
 
 router.post(
