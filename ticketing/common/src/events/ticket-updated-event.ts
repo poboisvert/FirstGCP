6136411b58117bd729bc2fa755ac99ea1e41fba2
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-Mac-mini.local>
2021-04-17 11:27:19 -0400

    Updates

--- ticketing/common/src/events/ticket-updated-event.ts
+++ ticketing/common/src/events/ticket-updated-event.ts
@@ -4,7 +4,7 @@ export interface TicketUpdatedEvent {
   subject: Subjects.TicketUpdated;
   data: {
     id: string;
-    version: number;
+    version: number; // ID
     title: string;
     price: number;
     userId: string;
